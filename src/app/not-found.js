b79a5f2c520e5b87da0d29d9ed1243a9f44d8016
baileydjamieson <baileyjamieson@jocke.tv>
2025-05-07 20:58:52 -0500

    Create not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you were looking for doesn’t exist.</p>
+    </div>
+  );
+}
