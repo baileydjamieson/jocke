2e557253f985eecb23011ef6b780d4d3c698060d
baileydjamieson <baileyjamieson@jocke.tv>
2025-05-07 20:59:35 -0500

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+export default function Layout({ children }) {
+    return (
+      <div>
+        <header>
+          <link rel="icon" href="C:\Users\baile\jocke\public\favicon.png"></link>
+        </header>
+        <main>{children}</main>
+      </div>
+    );
+  }
+  
